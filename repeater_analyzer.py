00f37bc0d27dff1ae98f1292c7af5e122159cc5e
idkidknow <ifidkidknow@gmail.com>
2018-11-02 15:06:33 +0800

    fixed #1

--- repeater_analyzer.py
+++ repeater_analyzer.py
@@ -5,48 +5,56 @@ _permitted_names = {
 _admin_names = set() # The set of the name of the administrators that can use "--clean" to clean the data.
 # (However, everyone can change his or her own name to one of the admin names, so this may be useless.)
 
-_sentences = dict() # 'sentence': count
-_repeaters = dict() # 'repeater': count
+class Group(object):
+    def __init__(self):
+        self.sentences = dict() # 'sentence': count
+        self.repeaters = dict() # 'repeater': count
+
+_groups = dict() # The dict of Group
 
 def message_filter(msg):
     """Return True if the message cannot be considered as a repeat."""
-    if msg == '[图片]' or '--' in msg or 'List:\n' in msg:
+    if msg == '' or '--' in msg or 'List:\n' in msg:
         return True
     return False
 
 def onQQMessage(bot, contact, member, content):
     if contact.name not in _permitted_names:
         return
-    counter(member.name, content)
+    if contact.name not in _groups:
+        _groups[contact.name] = Group()
+    counter(contact.name, member.name, content)
     
+    group = _groups[contact.name]
     if content == '--list repeaters':
         list_str = 'Repeaters List:'
-        for repeater in _repeaters.keys():
-            list_str += '\n' + repeater + ': ' + str(_repeaters[repeater]) + ' times.'
+        for repeater in group.repeaters.keys():
+            list_str += '\n' + repeater + ': ' + str(group.repeaters[repeater]) + ' times.'
         bot.SendTo(contact, list_str)
     if content == '--list sentences':
         list_str = 'Sentences List:'
-        for sentence in _sentences.keys():
-            if _sentences[sentence] == 0:
+        for sentence in group.sentences.keys():
+            if group.sentences[sentence] == 0:
                 continue
-            list_str += '\n「' + sentence + '」: ' + str(_sentences[sentence]) + ' times.'
+            list_str += '\n「' + sentence + '」: ' + str(group.sentences[sentence]) + ' times.'
         bot.SendTo(contact, list_str)
     if content == '--clean':
         if member.name not in _admin_names and not bot.isMe(contact, member):
             bot.SendTo(contact, 'Permission Denied.')
             return
-        _sentences.clear()
-        _repeaters.clear()
+        group.sentences.clear()
+        group.repeaters.clear()
         bot.SendTo(contact, 'Cleaned.')
 
-def counter(name, msg):
+def counter(group, name, msg):
     if message_filter(msg):
         return
-    if msg in _sentences:
-        _sentences[msg] += 1
-        if name in _repeaters:
-            _repeaters[name] += 1
+    group = _groups[group]
+    if msg in group.sentences:
+        group.sentences[msg] += 1
+        if name in group.repeaters:
+            group.repeaters[name] += 1
         else:
-            _repeaters[name] = 1
+            group.repeaters[name] = 1
     else:
-        _sentences[msg] = 0
+        group.sentences[msg] = 0
